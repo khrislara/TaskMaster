26bd33db8690be909349aaeef87af17b1f66b725
khris lara <nathanyel.kurayami.131@gmail.com>
2025-10-28 20:40:54 -0300

    Descripción de los cambios

--- app/src/main/java/com/example/taskmaster/TaskItem.java
+++ app/src/main/java/com/example/taskmaster/TaskItem.java
@@ -1,4 +1,19 @@
 package com.example.taskmaster;
 
 public class TaskItem {
+    private String description;
+    private int iconResourceId;
+
+    public TaskItem(String description, int iconResourceId) {
+        this.description = description;
+        this.iconResourceId = iconResourceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getIconResourceId() {
+        return iconResourceId;
+    }
 }
